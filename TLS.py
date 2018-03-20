a397792fe8c24044041488cc083325f510cb9e41
yaya <yayachen37@gmail.com>
2018-03-20 11:56:56 +0800

    feat : 更改 smooth function
    將變化大的地方竟量保持，相對同一個片段平滑

--- TLS.py
+++ TLS.py
@@ -23,10 +23,6 @@ def calVolumeDB(waveData, frameSize, overLap):
             volume[i] = 10*np.log10(np.sum(curFrame*curFrame)+0.000000000001)
         else:
             volume[i] = 10*np.log10(np.sum(curFrame*curFrame))
-        #try:
-           #volume[i] = 10*np.log10(np.sum(curFrame*curFrame))
-        #except:
-            #volume[i] = 10*np.log10(np.sum(curFrame*curFrame)+0.00000001)
     return volume
     #return np.mean(volume)
 """    
@@ -35,22 +31,25 @@ def replaceZeroes(data):
   data[data == 0] = min_nonzero
   return data
 """
+def Normalization(x): ## 0~1
+    return [((float(i)-min(x))/float(max(x)-min(x)))+1 for i in x]
 
+def Normalization2(x): ## -1~1
+    return [(float(i)-np.mean(x))/(max(x)-min(x)) for i in x]
 ####讀取wavedata(正規化)
 wavepath = '../10violin/wave/' 
 midipath = '../10violin/midi/' 
 outputpath = 'TLS_result/'
-song = '01-2'
+song = '02-2'
 sampFreq, snd = wavfile.read(wavepath+song+'.wav')
-waveData = snd*1.0/max(abs(snd))
-
+waveData = snd*1.0/max(abs(snd))#(snd-min(snd))/(max(snd)-min(snd))#snd*1.0/max(abs(snd))
+                                #(snd-np.mean(snd))/(max(snd)-min(snd))
 frameSize = 256
 overLap = 128
-smooth_size = 1
 
 ##讀取ground truth與小節音符個數
 import xml.etree.cElementTree as ET
-tree = ET.ElementTree(file=midipath+'01_violin.xml')
+tree = ET.ElementTree(file=midipath+song[0:2]+'_violin.xml')
 #for elem in tree.iterfind('part/measure'):
 #    print (elem.tag, elem.attrib)
 duration_dict = {'quarter':1,
@@ -129,24 +128,14 @@ Bar_onset = []
 
 
 for i in Bar:
-     #Barlen.append(int((offsets[BarCnt+i-1]-onsets[BarCnt])*sampFreq))   
-     #BarSum = BarSum + Barlen[-1]
-     #Bar_onset.append(BarSum)
-     #print(BarCnt[-1])
      Bar_onset.append(onsets[BarCnt[-1]])
      BarCnt.append(BarCnt[-1]+i)  
      
 ## 找出延長線有跨小節(扣除最後一小節05)     
 for i in range(len(BarCnt[:-1])):
-    print(i)
+    #print(i)
     for j in tieds:
          if BarCnt[i]-1 == j[0]:
-             #print("tied")
-             #print(BarCnt[i]-1)
-             #print(onsets[BarCnt[i]],onsets[BarCnt[i]-1])
-             #print(onsets[BarCnt[i]]-onsets[BarCnt[i]-1])
-             #print((onsets[BarCnt[i]]-onsets[BarCnt[i]-1])*(j[2]/(j[1]+j[2])))
-             #print(Bar_onset[i])
              Bar_onset[i]=Bar_onset[i]-(onsets[BarCnt[i]]-onsets[BarCnt[i]-1])*(j[2]/(j[1]+j[2]))
              #print(Bar_onset[i])
              
@@ -162,6 +151,7 @@ for i in range(len(onsets)):
 ###08
 #time_signature = 3
 track_level = min(time_value) #eighth note
+smooth_size = track_level
         
 ###03
 #time_signature = 4
@@ -198,101 +188,295 @@ for i in range(len(tl_len)):
   
 
 
+
+
+
 import pylab as plt
 import scipy.ndimage.filters as filters
 time = np.arange(0,len(waveData)/sampFreq,0.001)
 
-nv = np.interp(time,tl_len_cnt,v)
-nbpm = np.interp(time,tl_len_cnt,bpm)
-sigma = note_mean_len*1000*smooth_size#/2        ##window_size
-fv = filters.gaussian_filter1d(nv,sigma=sigma)
-fbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+##################
 
-#####onset###
-t_onsets = [int(tl_len_cnt[i]*1000)  for i in range(len(tl_len_cnt))]
-t_note_v= [nv[int(tl_len_cnt[i]*1000)]  for i in range(len(tl_len_cnt))]    
-t_note_bpm = [nbpm[int(tl_len_cnt[i]*1000)] for i in range(len(tl_len_cnt))]
-
-#####Bar###
-t_bar_v= [fv[int(Bar_onset[i]*1000)]  for i in range(len(Bar_onset))]    
-t_bar_bpm = [fbpm[int(Bar_onset[i]*1000)] for i in range(len(Bar_onset))]
+Nrmz = (Normalization(v),Normalization(bpm))
+#mN = np.mean(Nvf,dtype=np.float64)
+def Nrmz_smooth(N):
+    Nvf = []
+    Nvf.append(abs(N[0]-N[1])/N[0])
+    for i in range(1,len(N)-1):
+        Nvf.append((abs(N[i]-N[i-1])+abs(N[i]-N[i+1]))/2/N[i])
+    Nvf.append(abs(N[-1]-N[-2])/N[-1])
     
-
-plt.figure()
-
-plt.subplot(211)
-for i in Bar_onset:
-        plt.plot([i, i], [max(v), min(v)],'--b')
-plt.scatter(tl_len_cnt,t_note_v,s=10,label='onset')
-plt.plot(time,nv,label='loudness data')
-plt.plot(time,fv,label='smoothed : 1/4 Bar',linewidth=2)
-plt.ylabel("Loudness (dB)")
-plt.xlabel("time (s))")
-#plt.legend(loc='best')
-
-plt.subplot(212)
-for i in Bar_onset:
-        plt.plot([i, i], [max(bpm), min(bpm)],'--b')
-plt.scatter(tl_len_cnt,t_note_bpm,s=10,label='onset')
-plt.plot(time,nbpm,label='tempo data')
-plt.plot(time,fbpm,label='smoothed : 1/4 Bar',linewidth=2)
-plt.ylabel("Tempo (bpm)")
-plt.xlabel("time (s)")
-#plt.legend(loc='best')
-
-plt.savefig(outputpath+song+'T&L.png',dpi=300)
-plt.show()
-
-
-
-
-
-#plt.subplot(211)    
-plt.plot(fbpm, fv,color='orange',lw=2,zorder=2)
-
-T = [i for i in range(len(Bar_onset))]  
-plt.scatter(t_bar_bpm,t_bar_v,alpha=0.5,s=50,c =T,cmap=plt.cm.Oranges,zorder=2,label='Bar');
-plt.ylabel("Loudness (dB)")
-plt.xlabel("Tempo (bpm)")
-plt.colorbar()
-for i ,num in enumerate(range(len(Bar_onset))):
-    plt.annotate(num,(t_bar_bpm[i],t_bar_v[i]))   
-plt.legend()
-plt.savefig(outputpath+song+'TLS.png',dpi=300)
-plt.show()
-
-
+    plt.plot(N)
+    plt.plot(Nvf)
+    mN = np.mean(Nvf,dtype=np.float64)
+    x = [0,len(Nvf)]
+    y = [mN,mN]
+    plt.plot(x,y)
+    #plt.savefig('vN',dpi=300)
+    plt.show()
+    
+    x = [min(Nvf),mN,max(Nvf)]#[min(Nvf)-mN,mN,max(Nvf)-mN]
+    y = [track_level*2,track_level,track_level/2]
+    plt.plot(x,y)
+    plt.plot(mN,1,'*')
+    #plt.savefig('vR',dpi=300)
+    plt.show()
+    nnn = np.interp(Nvf,x,y)
+    #print(nnn)
+    #print(len(nnn))
+    seg = [True if v<mN else False for v in Nvf ]
+    return nnn,seg
+N_v,seg_v = Nrmz_smooth(Nrmz[0])
+N_b,seg_b = Nrmz_smooth(Nrmz[1])
 """
-
-frames = int(len(fv)/100)
-sbpm =[]
-sv= []
-for i in range(frames):
-    if i <= 10:
-        sbpm.append( fbpm[i*100])
-        sv.append(fv[i*100])
-    else:
-        sbpm.append(fbpm[(i-10)*100])
-        sv.append(fv[(i-10)*100])
-
-plt.scatter(sbpm,sv,alpha=0.5,s=50,zorder=2)
-plt.scatter(t_bar_bpm,t_bar_v,alpha=0.9,s=250,c =T,cmap=pl.cm.Oranges,zorder=2,label='Bar');
-plt.ylabel("Loudness (dB)")
-plt.xlabel("Tempo (bpm)")
-plt.colorbar()
-for i ,num in enumerate(range(len(Bar_onset))):
-    pl.annotate(num,(t_bar_bpm[i],t_bar_v[i]))   
-plt.legend()
+plt.plot(Nvf)
+mN = np.mean(Nvf,dtype=np.float64)
+x = [0,len(Nvf)]
+y = [mN,mN]
+plt.plot(x,y)
 plt.show()
+
+x = [min(Nvf)-mN,mN,max(Nvf)-mN]
+y = [track_level*2,track_level,track_level/2]
+plt.plot(x,y)
+plt.plot(mN,1,'*')
+plt.show()
+nnn = np.interp(Nvf,x,y)
 
 """
 
 
+
+def smooth_draw_original():
+    nv = np.interp(time,tl_len_cnt,v)
+    nbpm = np.interp(time,tl_len_cnt,bpm)
+    sigma = note_mean_len*1000*smooth_size/8        ##window_size
+    fv = filters.gaussian_filter1d(nv,sigma=sigma)#,mode='nearest')
+    fbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+    
+    #####onset###
+    #t_onsets = [int(tl_len_cnt[i]*1000)  for i in range(len(tl_len_cnt))]
+    t_note_v= [nv[int(tl_len_cnt[i]*1000)]  for i in range(len(tl_len_cnt))]    
+    t_note_bpm = [nbpm[int(tl_len_cnt[i]*1000)] for i in range(len(tl_len_cnt))]
+    
+    #####Bar###
+    t_bar_v= [fv[int(Bar_onset[i]*1000)]  for i in range(len(Bar_onset))]    
+    t_bar_bpm = [fbpm[int(Bar_onset[i]*1000)] for i in range(len(Bar_onset))]
+        
+    
+    plt.figure()
+    
+    plt.subplot(211)
+    for i in Bar_onset:
+            plt.plot([i, i], [max(v), min(v)],'--b')
+    plt.scatter(tl_len_cnt,t_note_v,s=10,label='onset')
+    plt.plot(time,nv,label='loudness data')
+    plt.plot(time,fv,label='smoothed : 1/4 Bar',linewidth=2)
+    plt.ylabel("Loudness (dB)")
+    plt.xlabel("time (s))")
+    #plt.legend(loc='best')
+    
+    plt.subplot(212)
+    for i in Bar_onset:
+            plt.plot([i, i], [max(bpm), min(bpm)],'--b')
+    plt.scatter(tl_len_cnt,t_note_bpm,s=10,label='onset')
+    plt.plot(time,nbpm,label='tempo data')
+    plt.plot(time,fbpm,label='smoothed : 1/4 Bar',linewidth=2)
+    plt.ylabel("Tempo (bpm)")
+    plt.xlabel("time (s)")
+    #plt.legend(loc='best')
+    
+    plt.savefig(outputpath+song+'T&L.png',dpi=300)
+    plt.show()
+
+
+
+    #plt.subplot(211)    
+    plt.plot(fbpm, fv,color='orange',lw=2,zorder=2)
+    
+    T = [i for i in range(len(Bar_onset))]  
+    plt.scatter(t_bar_bpm,t_bar_v,alpha=0.5,s=50,c =T,cmap=plt.cm.Oranges,zorder=2,label='Bar');
+    plt.ylabel("Loudness (dB)")
+    plt.xlabel("Tempo (bpm)")
+    plt.colorbar()
+    for i ,num in enumerate(range(len(Bar_onset))):
+        plt.annotate(num,(t_bar_bpm[i],t_bar_v[i]))   
+    plt.legend()
+    plt.savefig(outputpath+song+'TLS.png',dpi=300)
+    plt.show()
+    return fv,fbpm
+
+
+
+def smooth_draw_new():
+
+    nv = np.interp(time,tl_len_cnt,v)
+    nbpm = np.interp(time,tl_len_cnt,bpm)
+    sigma = note_mean_len*1000*smooth_size/8        ##window_size
+    tl_int = [ int(v*1000) for v in tl_len_cnt]
+    
+    
+    ############4
+    sigma = note_mean_len*1000*smooth_size/4
+    fv = filters.gaussian_filter1d(nv,sigma=sigma)#,mode='nearest')
+    fbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+    
+    sigma = note_mean_len*1000*smooth_size/2
+    
+    '''
+    for i in range(len(tl_int)):
+        if seg_v[i]:
+            p++
+        else:
+            fv[] = filters.gaussian_filter1d(nv,sigma=sigma)
+    '''
+    p=0;
+    i= 0;
+    while i<len(tl_int):
+        for j in range(i,len(tl_int)) :
+            if seg_v[j]:
+                p=p+1
+            else:
+                if p == 0:
+                    break
+                print(i ,i+p-1)
+                fv[tl_int[i]:tl_int[i+p-1]] = filters.gaussian_filter1d(fv[tl_int[i]:tl_int[i+p-1]],sigma=sigma)
+                i=i+p
+                p=0;
+                break
+        i=i+1;
+        
+    p=0;
+    i= 0;
+    while i<len(tl_int):
+        for j in range(i,len(tl_int)) :
+            if seg_b[j]:
+                p=p+1
+            else:
+                if p == 0:
+                    break
+                print(i ,i+p-1)
+                fbpm[tl_int[i]:tl_int[i+p-1]] = filters.gaussian_filter1d(fbpm[tl_int[i]:tl_int[i+p-1]],sigma=sigma)
+                i=i+p
+                p=0;
+                break
+        i=i+1;
+        
+    sigma = note_mean_len*1000*smooth_size/8
+    fv = filters.gaussian_filter1d(fv,sigma=sigma)#,mode='nearest')
+    fbpm = filters.gaussian_filter1d(fbpm,sigma=sigma)
+    ############3
+    '''
+    ttv =[[]]*len(tl_len_cnt)
+    ttbpm =[[]]*len(tl_len_cnt)
+
+    for i in range(len(tl_int)):
+        try:
+            sigma = note_mean_len*1000*N_v[i]/2
+            tv = filters.gaussian_filter1d(nv,sigma=sigma)
+            ttv[i] = tv
+            fv[tl_int[i]:tl_int[i+1]] = ttv[i][tl_int[i]:tl_int[i+1]]
+            
+            sigma = note_mean_len*1000*N_b[i]/2
+            tbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+            ttbpm[i] = tbpm
+            fbpm[tl_int[i]:tl_int[i+1]] = ttbpm[i][tl_int[i]:tl_int[i+1]]
+            
+        except IndexError:
+            sigma = note_mean_len*1000*N_v[i]/2
+            tv = filters.gaussian_filter1d(nv,sigma=sigma)
+            ttv[i] = tv
+            fv[tl_int[i]:len(nv)] = ttv[i][tl_int[i]:len(nv)]
+            #fv[tl_int[i]:len(nv)] = filters.gaussian_filter1d(nv[tl_int[i]:len(nv)],sigma=sigma)
+            sigma = note_mean_len*1000*N_b[i]/2
+            tbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+            ttbpm[i] = tbpm
+            fbpm[tl_int[i]:len(nv)] = ttbpm[i][tl_int[i]:len(nv)]
+            #fbpm[tl_int[i]:len(nv)] = filters.gaussian_filter1d(nbpm[tl_int[i]:len(nv)],sigma=sigma)
+    '''
+    
+    
+    ###############2
+    '''
+    for i in range(len(tl_int)):
+        try:
+            sigma = note_mean_len*1000*N_v[i]/10
+            fv[tl_int[i]:tl_int[i+1]] = filters.gaussian_filter1d(nv[tl_int[i]:tl_int[i+1]],sigma=sigma)
+            sigma = note_mean_len*1000*N_b[i]/10
+            fbpm[tl_int[i]:tl_int[i+1]] = filters.gaussian_filter1d(nbpm[tl_int[i]:tl_int[i+1]],sigma=sigma)
+        except IndexError:
+            sigma = note_mean_len*1000*N_v[i]/10
+            fv[tl_int[i]:len(nv)] = filters.gaussian_filter1d(nv[tl_int[i]:len(nv)],sigma=sigma)
+            sigma = note_mean_len*1000*N_b[i]/10
+            fbpm[tl_int[i]:len(nv)] = filters.gaussian_filter1d(nbpm[tl_int[i]:len(nv)],sigma=sigma)
+    '''
+    #fv = filters.gaussian_filter1d(nv,sigma=sigma)
+    #fbpm = filters.gaussian_filter1d(nbpm,sigma=sigma)
+    
+    
+    
+    #####onset###
+    #t_onsets = [int(tl_len_cnt[i]*1000)  for i in range(len(tl_len_cnt))]
+    t_note_v= [nv[int(tl_len_cnt[i]*1000)]  for i in range(len(tl_len_cnt))]    
+    t_note_bpm = [nbpm[int(tl_len_cnt[i]*1000)] for i in range(len(tl_len_cnt))]
+    
+    #####Bar###
+    t_bar_v= [fv[int(Bar_onset[i]*1000)]  for i in range(len(Bar_onset))]    
+    t_bar_bpm = [fbpm[int(Bar_onset[i]*1000)] for i in range(len(Bar_onset))]
+        
+    
+    plt.figure()
+    
+    plt.subplot(211)
+    for i in Bar_onset:
+            plt.plot([i, i], [max(v), min(v)],'--b')
+    plt.scatter(tl_len_cnt,t_note_v,s=10,label='onset')
+    plt.plot(time,nv,label='loudness data')
+    plt.plot(time,fv,label='smoothed : 1/4 Bar',linewidth=2)
+    plt.ylabel("Loudness (dB)")
+    plt.xlabel("time (s))")
+    #plt.legend(loc='best')
+    
+    plt.subplot(212)
+    for i in Bar_onset:
+            plt.plot([i, i], [max(bpm), min(bpm)],'--b')
+    plt.scatter(tl_len_cnt,t_note_bpm,s=10,label='onset')
+    plt.plot(time,nbpm,label='tempo data')
+    plt.plot(time,fbpm,label='smoothed : 1/4 Bar',linewidth=2)
+    plt.ylabel("Tempo (bpm)")
+    plt.xlabel("time (s)")
+    #plt.legend(loc='best')
+    
+    plt.savefig(outputpath+song+'T&L4.png',dpi=300)
+    plt.show()
+
+
+
+    #plt.subplot(211)    
+    plt.plot(fbpm, fv,color='orange',lw=2,zorder=2)
+    
+    T = [i for i in range(len(Bar_onset))]  
+    plt.scatter(t_bar_bpm,t_bar_v,alpha=0.5,s=50,c =T,cmap=plt.cm.Oranges,zorder=2,label='Bar');
+    plt.ylabel("Loudness (dB)")
+    plt.xlabel("Tempo (bpm)")
+    plt.colorbar()
+    for i ,num in enumerate(range(len(Bar_onset))):
+        plt.annotate(num,(t_bar_bpm[i],t_bar_v[i]))   
+    plt.legend()
+    plt.savefig(outputpath+song+'TLS4.png',dpi=300)
+    plt.show()
+    return fv,fbpm   
+
+fv,fbpm = smooth_draw_original()
+fv,fbpm = smooth_draw_new()
+
+
+"""
+
+########### animate ###########
 fig = pl.figure()
 ax1 = fig.add_subplot(1,1,1)
 ax2 = fig.add_subplot(1,1,1)
-
-
 
 
 ax = fig.add_subplot(1,1,1)
@@ -339,9 +523,9 @@ Bar_onset_s = [int(v*10) for v in Bar_onset]
 Bar_mean = int(note_mean_len*10)
 min_size = 3
 reduce_size = 10
-cur_size = (Bar_mean*reduce_size)+min_size
+cur_size = (10*reduce_size)+min_size#(Bar_mean*reduce_size)+min_size
 reduce_size = 10
-reduce_color = 1/(Bar_mean+0.01)
+reduce_color = 1/(10+0.01)#1/(Bar_mean+0.01)
 Bar_onset_s.append(len(TLS))
 
 
@@ -369,13 +553,13 @@ def animate(i):
     
     
             
-    '''
+    ##### reduce to min
     if i >= Bar_mean:
         TLS['size'][0:i-Bar_mean] -= reduce_size
         for j in range(i-Bar_mean):
             if TLS['size'][j] < min_size:
                 TLS['size'][j] = min_size
-    '''
+    
     
     scat.set_edgecolors(TLS['color'])
     scat.set_sizes(TLS['size'])
@@ -414,3 +598,4 @@ clip2 = AudioFileClip(wavepath+song+'.wav')
 new_video = clip1.set_audio(clip2)
 
 new_video.write_videofile(outputpath+song+'Tempo_Loudness_Space.mp4')
+"""
